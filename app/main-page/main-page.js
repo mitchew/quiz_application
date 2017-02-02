c6b4221450b4b5ea8453c359ce06061d09faf064
Mitchell Wilson <mitchell_e_w@yahoo.com>
2017-02-01 22:53:23 -0800

    can now add special characters to the text box

--- app/main-page/main-page.js
+++ app/main-page/main-page.js
@@ -12,4 +12,16 @@ angular.module('quizApp.main-page', ['ngRoute'])
 .controller('MainPageCtrl', ['$scope', function($scope) {
   $scope.lesson = "Lesson will change";
   $scope.question = "Test question here";
+  $scope.string = "Answer goes here";
+  $scope.questions = {
+    "questionSet1": {
+      "question1": {
+        "en":"apple",
+        "np":"timaanit"
+      }
+    }
+  };
+  $scope.addCharacter = function(char) {
+    return $scope.string += char;
+  };
 }]);
